286a3fac0ab9eccf3cc299c3100d8be2deb220e7
andmos <andreas.mosti@gmail.com>
2019-02-19 19:04:46 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,13 @@
+BikeshareFunction
+===
+
+[OpenFaaS](https://www.openfaas.com/) function for [BikeshareClient](https://github.com/andmos/BikeshareClient) access.
+
+```bash
+faas-cli build -f bikeshare-function.yml
+
+faas-cli deploy -f bikeshare-function.yml
+
+echo "johanneskirken" |faas-cli invoke bikeshare-function
+# Bikes available: 1, Locks available: 23
+```
---
+++ bikeshare-function/FunctionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using BikeshareClient;
+using BikeshareClient.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        private IBikeshareClient bikeshareClient;
+        public FunctionHandler()
+        {
+            var gbfsAddress = System.Environment.GetEnvironmentVariable("GBFSAddress");
+            bikeshareClient = new Client(gbfsAddress);
+        }
+        public string Handle(string input) 
+        {
+            if(string.IsNullOrEmpty(input))
+            {
+                return "Requires BikeShare station name";
+            }
+            var stationId = GetStationId(input, bikeshareClient.GetStationsAsync().Result);
+            var stationStatus = GetStationStatus(stationId);
+
+            return $"Bikes available: {stationStatus.BikesAvailable}, Locks available: {stationStatus.DocksAvailable}";
+        }
+
+        private string GetStationId(string stationName, IEnumerable<Station> stations)
+        {
+            return stations.SingleOrDefault(s => s.Name.ToLower().Equals(stationName.ToLower().Trim())).Id; 
+        }
+
+        private StationStatus GetStationStatus(string stationId)
+        {
+            var stations = bikeshareClient.GetStationsStatusAsync().Result;
+            return stations.FirstOrDefault(s => s.Id.Equals(stationId));
+        }
+    }
+}
