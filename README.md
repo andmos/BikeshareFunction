c063dba4799a47b84a1f87536900f37a751fdfd4
andmos <andreas.mosti@gmail.com>
2019-05-11 14:05:00 +0200

    added function for parsing the GBFS systems csv file

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ BikeshareFunction
 [OpenFaaS](https://www.openfaas.com/) function for [BikeshareClient](https://github.com/andmos/BikeshareClient) access.
 Provides a simple serverless [GBFS](https://github.com/NABSA/gbfs) status system.
 
+`gbfs-sysmtems-function` parses the [GBFS Systems CSV](https://raw.githubusercontent.com/NABSA/gbfs/master/systems.csv) to JSON, providing API for systems overview.
+
 ```bash
 faas-cli template pull https://github.com/burtonr/csharp-kestrel-template
 faas-cli build -f bikeshare-function.yml
@@ -12,6 +14,16 @@ faas-cli deploy -f bikeshare-function.yml
 
 echo "johanneskirken" |faas-cli invoke bikeshare-function
 # Bikes available: 1, Locks available: 23
+
+echo |faas-cli invoke gbfs-systems-function |jq
+#{
+#    "CountryCode": "US",
+#    "Name": "Sobi Long Beach",
+#    "Location": "Long Beach, NY",
+#    "Id": "sobi_long_beach",
+#    "Url": "http://sobilongbeach.com/",
+#    "GBFSFileUrl": "http://sobilongbeach.com/opendata/gbfs.json"
+#  }
 ```
 
 Change GBFS system by updating the `GBFSAddress` variable:
---
+++ gbfs-systems-function/FunctionHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        private readonly Uri _gbfsSystemsFile = new Uri(@"https://raw.githubusercontent.com/NABSA/gbfs/master/systems.csv");
+        private readonly HttpClient _httpClient;
+        public FunctionHandler()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task<string> Handle(string input) 
+        {
+            
+            List<GbfsSystem> gbfsSystems = new List<GbfsSystem>();
+            using(var reader = new StreamReader(await _httpClient.GetStreamAsync(_gbfsSystemsFile)))
+            {
+                using(var csv = new CsvReader(reader))
+                {
+                    gbfsSystems = csv.GetRecords<GbfsSystem>().ToList();
+                }
+            }
+            return JsonConvert.SerializeObject(gbfsSystems);
+            
+        }
+        public class GbfsSystem
+        {
+            [Name("Country Code")]
+            public string CountryCode {get; set;} 
+            [Name("Name")]
+            public string Name {get; set;}
+            [Name("Location")]
+            public string Location {get; set;}
+            [Name("System ID")]
+            public string Id {get; set;}
+            [Name("URL")]
+            public string Url {get; set;}
+            [Name("Auto-Discovery URL")]
+            public string GBFSFileUrl {get; set;}  
+        }
+    }
+}
