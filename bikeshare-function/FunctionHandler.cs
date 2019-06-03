9e49c7e692fc46a70e745f11080c155cfdf6e97c
andmos <andreas.mosti@gmail.com>
2019-06-03 15:21:39 +0200

    Updated function with support for listing all stations

--- bikeshare-function/FunctionHandler.cs
+++ bikeshare-function/FunctionHandler.cs
@@ -12,11 +12,11 @@ namespace Function
 {
     public class FunctionHandler
     {
-        private IBikeshareClient bikeshareClient;
+        private IBikeshareClient _bikeshareClient;
         public FunctionHandler()
         {
             var gbfsAddress = System.Environment.GetEnvironmentVariable("GBFSAddress");
-            bikeshareClient = new Client(gbfsAddress);
+            _bikeshareClient = new Client(gbfsAddress);
         }
         public async Task<string> Handle(string input) 
         {
@@ -24,24 +24,52 @@ namespace Function
             {
                 return "Requires BikeShare station name";
             }
-            var stationId =  GetStationId(input, await bikeshareClient.GetStationsAsync());
-            var stationStatus = await GetStationStatus(stationId);
-            dynamic returnableStationStatus = new JObject();
-            returnableStationStatus.BikesAvailable = stationStatus.BikesAvailable;
-            returnableStationStatus.LocksAvailable = stationStatus.DocksAvailable;
-
-            return JsonConvert.SerializeObject(returnableStationStatus);
+            
+            var stationStatus = await GetAllBikeStationStatus();
+            if(input.ToLower().Equals("all") || input.ToLower().Equals("list") || input.ToLower().Equals("stations"))
+            {
+                return JsonConvert.SerializeObject(stationStatus);
+            }
+            stationStatus = stationStatus.Where(station => station.Name.ToLower().Equals(input.ToLower()));
+            return JsonConvert.SerializeObject(stationStatus.FirstOrDefault());
         }
 
+        private async Task<IEnumerable<BikeshareFunctionStationStatus>> GetAllBikeStationStatus()
+        {
+            var bikeFunctionStationStatus = new List<BikeshareFunctionStationStatus>();
+            var stations = await _bikeshareClient.GetStationsAsync();
+            var stationStatus =  await _bikeshareClient.GetStationsStatusAsync();
+            
+            foreach(var station in stations)
+            {
+                var singleStationStatus = GetStationStatus(GetStationId(station.Name, stations), stationStatus);
+                {
+                    bikeFunctionStationStatus.Add(new BikeshareFunctionStationStatus 
+                    {
+                        Name = station.Name,
+                        BikesAvailable = singleStationStatus.BikesAvailable,
+                        LocksAvailable = singleStationStatus.DocksAvailable
+                        
+                    });
+                }
+            }
+            return bikeFunctionStationStatus;
+        }
         private string GetStationId(string stationName, IEnumerable<Station> stations)
         {
             return stations.SingleOrDefault(s => s.Name.ToLower().Equals(stationName.ToLower().Trim())).Id; 
         }
-
-        private async Task<StationStatus> GetStationStatus(string stationId)
+        private StationStatus GetStationStatus(string stationId, IEnumerable<StationStatus> stationStatus)
         {
-            var stations = await bikeshareClient.GetStationsStatusAsync();
-            return stations.FirstOrDefault(s => s.Id.Equals(stationId));
+            return stationStatus.FirstOrDefault(s => s.Id.Equals(stationId));
         }
+
+        private class BikeshareFunctionStationStatus
+        {
+            public string Name {get; set;}
+            public int BikesAvailable {get; set;}
+            public int LocksAvailable {get; set;}
+        }
+
     }
 }
